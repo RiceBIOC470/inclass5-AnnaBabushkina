8816a99e1c3a166437675ea7eae9ce8e8c9d946a
warmflasha <warmflasha@gmail.com>
2019-09-10 10:28:33 -0500

    Added inclass5

---
+++ inclass5.m
@@ -0,0 +1,21 @@
+%Inclass assignment 5. 
+
+% 1. Write a function that reads in an image and returns the intensity
+% values of the brightest and dimest pixels as well as the indices of those 
+% pixels - for example, for the max, the function should return ind_max1, 
+% ind_max2, and max_value so that: 
+% img(ind_max1,ind_max2) = max_value. 
+% Get any black and white image and run this function on it (or get a color 
+% image and convert to black and white - hint: take only one of the three 
+% color channels).
+
+
+% 2. (a) Write a function that reads in an image and displays it so that 1%
+% of the pixels are black and 1% of the pixels are white. Hint: your
+% function can call the builtin stretchlim function, see the help for that function.
+% (b) Write a second function where the fraction of pixels to display as
+% black or white are equal and can be specified as a second input to this
+% function. (c) Redo part (b) but make the option to specify the fraction
+% of black/white pixels optional with a default value of 1% if not
+% specified.  Run your functions on the image you used for 1. 
+
